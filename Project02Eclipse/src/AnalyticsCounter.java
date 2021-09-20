cfefff10add9c91388779f38d9fe9b7b1a6c70d9
Eric Bulot <eric.bulot@gmail.com>
2021-09-20 18:12:38 +0200

    Création d'une liste

--- Project02Eclipse/src/AnalyticsCounter.java
+++ Project02Eclipse/src/AnalyticsCounter.java
@@ -1,23 +1,24 @@
-/**  Main file Cleaning **/
-
-
-
-
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
 
 
     public static void main(String args[]) throws Exception {
-        // first get input
-        BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-        String line = reader.readLine();
+        /* List creation */
 
+        com.hemebiotech.analytics.ISymptomReader symptomReader = new com.hemebiotech.analytics.ReadSymptomDataFromFile("symptoms.txt");
+        List<String> symptoms = symptomReader.GetSymptoms();
 
+        /* Result control */
+
+			for (int i = 0; i < symptoms.size(); i++) {
+			System.out.println(symptoms.get(i));
+		}
 
 
     }
